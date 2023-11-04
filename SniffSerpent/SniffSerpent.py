86f6ad46bc00466e5c6a8bb9a9eb3382dc0b970f
Shay Hahiashvili <shayhh@outlook.com>
2023-11-04 18:04:06 +0200

    comments

--- SniffSerpent/SniffSerpent.py
+++ SniffSerpent/SniffSerpent.py
@@ -891,7 +891,7 @@ class PacketSniffer(QMainWindow):
         else: #else ip is empty so its not specified by user (optional)
             self.validIp = True #set the validIp flag to true
         if self.validIp: #if ip is valid we set the default style of the edit line lable
-            style = 'background-color: rgba(32,33,35,255); border-radius: 15px; border-style: outset; border-width: 2px; border-radius: 15px; border-color: black;	padding: 4px;'
+            style = 'background-color: rgba(32,33,35,255); border-radius: 15px; border-style: outset; border-width: 2px; border-radius: 15px; border-color: black; padding: 4px;'
             self.IPLineEdit.setStyleSheet(style)
         else: #else the user input is invalid, we show a red border on the edit line lable for error indication
             style = 'background-color: rgba(32,33,35,255); border-radius: 15px; border-style: outset; border-width: 2px; border-radius: 15px; border-color: rgb(139,0,0); padding: 4px;'
